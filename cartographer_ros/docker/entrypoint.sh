9199970c650e7c75ed523b11db69642b02e7d2b2
Rodrigue de Schaetzen <rodrigue.de.schaetzen@hotmail.com>
2018-09-14 17:49:58 +0200

    Added Dockerfile and entrypoint

---
+++ cartographer_ros/docker/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+echo "Starting cartographer_ros with ROS_MASTER_URI=$ROS_MASTER_URI"
+
+source /opt/ros/kinetic/setup.bash
+source /opt/cartographer_ros/setup.bash
+
+if [ "$CARTO_MODE" = "localization" ]; then
+     wget -O /current_map_state.pbstream "$CARTO_MAP"
+     roslaunch cartographer_ros turtlebot3_localization.launch load_state_filename:=/current_map_state.pbstream
+else
+     roslaunch cartographer_ros turtlebot3_slam.launch
+fi
+
+exec "$@"
