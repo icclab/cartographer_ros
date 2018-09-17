0b8cad66f2f82da10e6c49132714a97ce74cedd3
Rodrigue de Schaetzen <rodrigue.de.schaetzen@hotmail.com>
2018-09-17 16:32:29 +0200

    Set entrypoint for debugging

--- cartographer_ros/docker/entrypoint.sh
+++ cartographer_ros/docker/entrypoint.sh
@@ -6,11 +6,15 @@ echo "Starting cartographer_ros with ROS_MASTER_URI=$ROS_MASTER_URI"
 source /opt/ros/kinetic/setup.bash
 source /opt/cartographer_ros/setup.bash
 
-if [ "$CARTO_MODE" = "localization" ]; then
-     wget -O /current_map_state.pbstream "$CARTO_MAP"
-     roslaunch cartographer_ros turtlebot3_localization.launch load_state_filename:=/current_map_state.pbstream
-else
-     roslaunch cartographer_ros turtlebot3_slam.launch
-fi
+#if [ "$CARTO_MODE" = "localization" ]; then
+#     wget -O /current_map_state.pbstream "$CARTO_MAP"
+#     roslaunch cartographer_ros turtlebot($TURTLEBOT_NUM)_localization.launch load_state_filename:=/current_map_state.pbstream 
+#else
+#     roslaunch cartographer_ros turtlebot($TURTLEBOT_NUM)_slam.launch
+#fi
+
+#roslaunch cartographer_ros turtlebot3_slam.launch
+
+sleep infinity
 
 exec "$@"
