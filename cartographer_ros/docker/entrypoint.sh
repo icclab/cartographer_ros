8b5f2f2bb92f4bd516857767ef3deca4236326df
Rodrigue de Schaetzen <rodrigue.de.schaetzen@hotmail.com>
2018-09-18 15:05:39 +0200

    Container launches carto config + launch files based on tb model

--- cartographer_ros/docker/entrypoint.sh
+++ cartographer_ros/docker/entrypoint.sh
@@ -6,15 +6,9 @@ echo "Starting cartographer_ros with ROS_MASTER_URI=$ROS_MASTER_URI"
 source /opt/ros/kinetic/setup.bash
 source /opt/cartographer_ros/setup.bash
 
-#if [ "$CARTO_MODE" = "localization" ]; then
-#     wget -O /current_map_state.pbstream "$CARTO_MAP"
-#     roslaunch cartographer_ros turtlebot($TURTLEBOT_NUM)_localization.launch load_state_filename:=/current_map_state.pbstream 
-#else
-#     roslaunch cartographer_ros turtlebot($TURTLEBOT_NUM)_slam.launch
-#fi
-
-#roslaunch cartographer_ros turtlebot3_slam.launch
-
-sleep infinity
-
-exec "$@"
+if [ "$CARTO_MODE" = "localization" ]; then
+     wget -O /current_map_state.pbstream "$CARTO_MAP"
+     roslaunch cartographer_ros turtlebot${MODEL_NUM}_localization.launch load_state_filename:=/current_map_state.pbstream
+else
+     roslaunch cartographer_ros turtlebot${MODEL_NUM}_slam.launch
+fi
