24a81dd84014e9e6da1349ec45c8bae258e91f27
Lukasz Janasz <fitter22@hotmail.com>
2019-10-22 13:46:53 +0200

    further fixes for entrypoint

--- cartographer_ros/docker/entrypoint.sh
+++ cartographer_ros/docker/entrypoint.sh
@@ -3,12 +3,12 @@ set -e
 
 echo "Starting cartographer_ros with ROS_MASTER_URI=$ROS_MASTER_URI"
 
-source /opt/ros/melodic/setup.bash
+source /opt/ros/$ROS_DISTRO/setup.bash
 source /opt/cartographer_ros/setup.bash
 
 if [ "$CARTO_MODE" = "localization" ]; then
      wget -O /current_map_state.pbstream "$CARTO_MAP"
-     roslaunch cartographer_ros turtlebot${MODEL}_localization.launch load_state_filename:=/current_map_state.pbstream
+     roslaunch cartographer_ros ${MODEL}_localization.launch load_state_filename:=/current_map_state.pbstream
 else
-     roslaunch cartographer_ros turtlebot${MODEL}_slam.launch
+     roslaunch cartographer_ros ${MODEL}_slam.launch
 fi
